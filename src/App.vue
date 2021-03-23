e8e50ab6118cad86b34723532e2497c9a2f837f6
christianSeong <christian199@naver.com>
2021-03-23 21:44:55 +0900

    Todo list 첫번째 커밋

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,56 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+   <h1 class= "text-center">Todo App</h1>
+
+   <input 
+   type="text" 
+   class="w-100 p-2  " 
+   placeholder="할 일을 적으세요!"
+   @keyup.enter="addToDo"
+   >
+
+
+
+<hr>
+ 
+   <Todo 
+   v-for="todo in todos" 
+   :key = "todo.id"
+   :todo = "todo"
+   />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+import Todo from '@/components/Todo.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Todo
+  },
+
+  data(){
+    return {
+      todos : [
+        {id : 1, text : "커피 마시기", checked :false},
+        {id : 2, text : "고양이 밥 주기", checked :true},
+        {id : 3, text : "코딩 하기", checked :false},
+      ]
+    }
+  },
+
+  methods : {
+    addToDo(e){
+      //console.log(e.target.value)
+      this.todos.push({
+        id : Math.random(),
+        text : e.target.value,
+        checked: false
+      })
+
+      console.log(this.todos)
+    }
   }
 }
 </script>
---
+++ src/components/Todo.vue
@@ -0,0 +1,30 @@
+<template>
+     <div>
+         <input type="checkbox" :checked = "todo.checked">
+         <span class="ms-3">{{todo.text}}</span>
+        
+    </div>
+</template>
+
+<script>
+
+export default {
+  
+  data(){
+      return {
+
+      }
+  },
+  props : {
+      todo : {
+          type : Object,
+          required : true
+      }
+  }
+    
+}
+</script>
+
+<style scoped>
+
+</style>
