4b5701c71b4aba151aaa43b910dde8e4eefb45cd
DESKTOP-NH8QCV7\arbut <arbutus@outlook.kr>
2021-03-27 01:01:29 +0900

    update 1

--- todo/src/components/HomeComponent/EventBanner.vue
+++ todo/src/components/HomeComponent/EventBanner.vue
@@ -1,13 +1,12 @@
 <template>
   <div class="eventbanner_tab">
-      <tiny-slider :mouse-drag="true" :loop="false" items="2" gutter="20">
+      <tiny-slider :mouse-drag="true" :loop="false" items="1" gutter="20" navPosition="bottom" controlsText="<>">
         <img src="https://picsum.photos/1024/480/?image=10"/>
         <img src="https://picsum.photos/1024/480/?image=10"/>
         <img src="https://picsum.photos/1024/480/?image=10"/>
         <img src="https://picsum.photos/1024/480/?image=10"/>
         <img src="https://picsum.photos/1024/480/?image=10"/>
         <img src="https://picsum.photos/1024/480/?image=10"/>
-
         <img src="https://picsum.photos/1024/480/?image=10"/>
         <img src="https://picsum.photos/1024/480/?image=10"/>
 
@@ -28,5 +27,7 @@ export default {
 </script>
 
 <style>
-
+  .eventbanner_tab {
+    overflow: hidden;
+  }
 </style>
